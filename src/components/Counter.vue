249ceeff8d7442d01cf4862601d144d6010f8e77
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-02-19 10:19:54 +0700

    first

---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Welcome to Counter</h2>
+    {{ counter }}
+    <br />
+    <button @click="incrementCounter">increment counter</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      incrementCounter() {
+        this.$store.dispatch('incrementAction',1)
+      }
+    },
+
+    computed: {
+      counter() {
+        return this.$store.getters.counter
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
